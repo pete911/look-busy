6daeb641e35346fbcec457b290e1cb28ac694d88
peter.reisinger <peter.reisinger@houseful.co.uk>
2025-09-26 08:56:48 +0100

    initial commit

---
+++ esp32-s3-nano/README.md
@@ -0,0 +1,4 @@
+# ESP32-S3-Nano
+
+Instructions for [ESP32-S3-Nano](https://www.waveshare.com/wiki/ESP32-S3-Nano) board.
+Follow the instructions to download and set up arduino ide.
---
+++ esp32-s3-nano/mouse.ino
@@ -0,0 +1,43 @@
+#include <Arduino.h>
+#include "USB.h"
+#include "USBHIDMouse.h"
+#include "USBHIDKeyboard.h"
+
+USBHIDMouse Mouse;
+USBHIDKeyboard Keyboard;
+
+int rndX;
+int rndY;
+int rndDelaySec;
+
+void setup() {
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
+    Mouse.begin();
+}
+
+void loop() {
+  blink(3);
+  rndX = random(500, 1000);
+  rndY = random(500, 1000);
+  rndDelaySec = random(5, 15);
+  moveAndReturn(rndX, rndY);
+  blink(rndDelaySec);
+  delay(rndDelaySec * 1000);
+}
+
+void moveAndReturn(int x, int y) {
+  Mouse.move(x, y, 0);
+  delay(1000);
+  Mouse.move(-1 * x, -1 * y, 0);
+}
+
+void blink(int times) {
+  for (int i = 0; i <= times; i++) {
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(50);
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(50);
+  }
+}
+
